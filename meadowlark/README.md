ea174eb0b33019725ca4c144113bcb37c45162a6
贺迎春 <yingchun.he@100credit.com>
2019-04-11 14:07:28 +0800

    添加学习记录

--- meadowlark/README.md
+++ meadowlark/README.md
@@ -10,3 +10,15 @@
 
 `node index.js`
 `npx nodemon index.js` - 由于`nodemon`并不是安装的全局的，所以要这样启动。具体可查看[nodemon](https://github.com/remy/nodemon)
+
+## 手动重启
+
+`rs`
+
+## jshint
+
+`npx jshint index.js`
+
+## package 记录
+
+- body-parser 来解析 post 请求 URL 编码体
--- meadowlark/index.js
+++ meadowlark/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var fortune = require("./lib/fortunes.js");
+
+var weather = require("./src/partials/weather.js");
 var handlebars = require("express3-handlebars").create({
   defaultLayout: "main"
 });
@@ -9,35 +11,52 @@ app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
 
+// 针对每个路径，要放在前面
+app.use(function(req, res, next) {
+  if (!res.locals.partials) {
+    res.locals.partials = {};
+  }
+  res.locals.partials.weather = weather.getWeatherData();
+  console.log(res.locals.partials.weather);
+  next();
+});
 app.get("/", (req, res) => {
   // res.type("text/plain");
   // res.send("Meadowlark Travel");
   res.render("home");
 });
 app.get("/about", (req, res) => {
-  // res.type("text/plain");
-  // res.send("About Meadowlark Travel");
+  // 使用另外的模板文件
+  res.render("about", { fortune: fortune.getFortune(), layout: "microsite" });
+});
 
-  res.render("about", { fortune: fortune.getFortune() });
+// 返回一个接口，jsonp的数据
+app.get("/about.do", (req, res) => {
+  res.send('callback({name:"dezhao"})');
+});
+
+app.get("/test", (req, res) => {
+  res.type("text/plain");
+  res.send("this is a test");
+});
+
+app.get("/main-layout", (req, res) => {
+  res.render("home", { layout: null });
+});
+
+app.get("/api/tour/:id", (req, res) => {
+  res.json({ success: true });
 });
 
 app.use(function(req, res) {
-  // res.type("text/plain");
-  // res.status(404);
-  // res.send("404 - Not Found");
-  res.status(404);
-  res.render("404");
+  res.status(404).render("404");
 });
 
 app.use(function(err, req, res, next) {
-  // console.error(err.stack);
-  // res.type("text/plain");
-  // res.status(500);
-  // res.send("500 - Server Error");
   console.error(err.stack);
-  res.status(500);
-  res.render("500");
+  res.status(500).render("500");
 });
+
 app.use(express.static(__dirname + "/public"));
 
 app.listen(app.get("port"), function() {
